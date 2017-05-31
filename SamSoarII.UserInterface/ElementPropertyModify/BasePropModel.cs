5a77b3a675da7dd017cc2b77d171cecc2095ba23
chengrui wang <morenan@126.com>
2017-05-31 10:57:33 +0800

    New ElementPropertyDialog

---
+++ SamSoarII.UserInterface/ElementPropertyModify/BasePropModel.cs
@@ -0,0 +1,40 @@
+﻿using SamSoarII.ValueModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SamSoarII.UserInterface
+{
+    public abstract class BasePropModel : UserControl
+    {
+        public abstract string InstructionName { get; set; }
+        public abstract int Count { get; set; }
+        public abstract string ValueString1 { get; set; }
+        public abstract string ValueString2 { get; set; }
+        public abstract string ValueString3 { get; set; }
+        public abstract string ValueString4 { get; set; }
+        public abstract string ValueString5 { get; set; }
+        public virtual string CommentString1 { get; set; }
+        public virtual string CommentString2 { get; set; }
+        public virtual string CommentString3 { get; set; }
+        public virtual string CommentString4 { get; set; }
+        public virtual string CommentString5 { get; set; }
+
+        protected void UpdateComment(string valuestring)
+        {
+            if (ValueString1.Equals(valuestring))
+                CommentString1 = ValueCommentManager.GetComment(valuestring);
+            if (ValueString2.Equals(valuestring))
+                CommentString2 = ValueCommentManager.GetComment(valuestring);
+            if (ValueString3.Equals(valuestring))
+                CommentString3 = ValueCommentManager.GetComment(valuestring);
+            if (ValueString4.Equals(valuestring))
+                CommentString4 = ValueCommentManager.GetComment(valuestring);
+            if (ValueString5.Equals(valuestring))
+                CommentString5 = ValueCommentManager.GetComment(valuestring);
+        }
+    }
+}
---
+++ SamSoarII.UserInterface/ElementPropertyModify/OutRecPropModel.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SamSoarII.UserInterface
+{
+    /// <summary>
+    /// OutRecPropModel.xaml 的交互逻辑
+    /// </summary>
+    public partial class OutRecPropModel : BasePropModel
+    {
+        public OutRecPropModel()
+        {
+            InitializeComponent();
+        }
+        
+        private string instname;
+        public override string InstructionName
+        {
+            get
+            {
+                return this.instname;
+            }
+            set
+            {
+                this.instname = value;
+                TopTextBlock.Text = instname;
+            }
+        }
+
+        private int count;
+        public override int Count
+        {
+            get
+            {
+                return this.count;
+            }
+            set
+            {
+                this.count = value;
+                MiddleTextBox1.Visibility = count >= 1
+                    ? Visibility.Visible
+                    : Visibility.Hidden;
+                MiddleTextBox2.Visibility = count >= 2
+                    ? Visibility.Visible
+                    : Visibility.Hidden;
+                MiddleTextBox3.Visibility = count >= 3
+                    ? Visibility.Visible
+                    : Visibility.Hidden;
+                MiddleTextBox4.Visibility = count >= 4
+                    ? Visibility.Visible
+                    : Visibility.Hidden;
+                MiddleTextBox5.Visibility = count >= 5
+                    ? Visibility.Visible
+                    : Visibility.Hidden;
+            }
+        }
+
+        public override string ValueString1
+        {
+            get
+            {
+                return MiddleTextBox1.Text;
+            }
+            set
+            {
+                MiddleTextBox1.Text = value;
+                UpdateComment(value);
+            }
+        }
+        public override string ValueString2
+        {
+            get
+            {
+                return MiddleTextBox2.Text;
+            }
+            set
+            {
+                MiddleTextBox2.Text = value;
+                UpdateComment(value);
+            }
+        }
+        public override string ValueString3
+        {
+            get
+            {
+                return MiddleTextBox3.Text;
+            }
+            set
+            {
+                MiddleTextBox3.Text = value;
+                UpdateComment(value);
+            }
+        }
+        public override string ValueString4
+        {
+            get
+            {
+                return MiddleTextBox4.Text;
+            }
+            set
+            {
+                MiddleTextBox4.Text = value;
+                UpdateComment(value);
+            }
+        }
+        public override string ValueString5
+        {
+            get
+            {
+                return MiddleTextBox5.Text;
+            }
+            set
+            {
+                MiddleTextBox5.Text = value;
+                UpdateComment(value);
+            }
+        }
+
+        private void MiddleTextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateComment(MiddleTextBox1.Text);
+        }
+        private void MiddleTextBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateComment(MiddleTextBox2.Text);
+        }
+        private void MiddleTextBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateComment(MiddleTextBox3.Text);
+        }
+        private void MiddleTextBox4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateComment(MiddleTextBox4.Text);
+        }
+        private void MiddleTextBox5_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateComment(MiddleTextBox5.Text);
+        }
+    }
+}
