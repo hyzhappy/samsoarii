4733b0a98436673a2921a9a294ce7710d603188c
chengrui wang <morenan@126.com>
2017-06-15 17:53:57 +0800

    Fixed bug

--- SamSoarII/UI/MainWindow.xaml.cs
+++ SamSoarII/UI/MainWindow.xaml.cs
@@ -921,6 +921,14 @@ namespace SamSoarII.AppMain.UI
         }
         private void OnNewProjectExecute(object sender, RoutedEventArgs e)
         {
+            if (_interactionFacade.ProjectModel.LadderMode == LadderMode.Simulate)
+            {
+                OnSimulateCommandExecute(sender, e);
+            }
+            if (_interactionFacade.ProjectModel.LadderMode == LadderMode.Monitor)
+            {
+                OnMonitorCommandExecute(sender, e);
+            }
             CurrentProjectHandle(true, false);
         }
         private void ProjectOpen()
@@ -942,6 +950,14 @@ namespace SamSoarII.AppMain.UI
         }
         private void OnOpenProjectExecute(object sender, RoutedEventArgs e)
         {
+            if (_interactionFacade.ProjectModel.LadderMode == LadderMode.Simulate)
+            {
+                OnSimulateCommandExecute(sender, e);
+            }
+            if (_interactionFacade.ProjectModel.LadderMode == LadderMode.Monitor)
+            {
+                OnMonitorCommandExecute(sender, e);
+            }
             CurrentProjectHandle(false, true);
         }
 
