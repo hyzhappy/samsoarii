b1e636976be9e9d9293fc6e2c278c47577608ca5
chengrui wang <morenan@126.com>
2017-06-19 18:02:33 +0800

    Encrypt

--- SamSoarII/Communication/Command/Download80Command.cs
+++ SamSoarII/Communication/Command/Download80Command.cs
@@ -25,9 +25,29 @@ namespace SamSoarII.Communication.Command
 
         public Download80Command(int id, byte[] data)
         {
-            bytes = new byte[] { (byte)(id&0xff) }
+            Encrypt(data);
+            bytes = new byte[] { 0x80, (byte)(id&0xff) }
                 .Concat(data).ToArray();
             bytes = bytes.Concat(CRC16.GetCRC(bytes)).ToArray();
         }
+        
+        private void Encrypt(byte[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                byte di = data[i];
+                byte bs = 0x01;
+                int os = 7;
+                data[i] = 0;
+                while (os >= -7)
+                {
+                    data[i] |= (byte)(os > 0 ? ((di & bs) << os) : ((di & bs) >> (-os)));
+                    bs <<= 1; os -= 2;
+                }
+                data[i] ^= 0x02;
+            }
+        }
+
+        
     }
 }
