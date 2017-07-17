6226ca1c69f0a8cec0858d8457c7bcd6ce0965e0
chengrui wang <morenan@126.com>
2017-07-17 10:12:57 +0800

    Delete All

--- SamSoarII/Communication/Util/usbdll/gen_usbdll.bat
+++
@@ -1,2 +0,0 @@
-i686-w64-mingw32-gcc -c -DBUILD_DLL usbdll.c -o usbdll.o
-i686-w64-mingw32-gcc -shared -o plcusb.dll usbdll.o libusb-1.0.dll -Wl,--kill-at
--- SamSoarII/simug/gen_simu.bat
+++
@@ -1,2 +0,0 @@
-i686-w64-mingw32-gcc -c -DBUILD_DLL simug.c -o simug.o
-i686-w64-mingw32-gcc -shared -o ../simu.dll simug.o -Wl,--kill-at
