f135a1bb7ac0266ef11c22b61dd0f4f8c9ab1826
chengrui wang <morenan@126.com>
2017-07-21 09:15:12 +0800

    Breakpoint

---
+++ SamSoarII/Global/BreakpointCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace SamSoarII.Global
+{
+    class BreakpointCommand
+    {
+        public static RoutedUICommand Add;
+        public static RoutedUICommand Setting;
+        public static RoutedUICommand Remove;
+        public static RoutedUICommand JumpTo;
+
+        static BreakpointCommand()
+        {
+            Add = new RoutedUICommand();
+            Setting = new RoutedUICommand();
+            Remove = new RoutedUICommand();
+            JumpTo = new RoutedUICommand();
+        }
+
+    }
+}
