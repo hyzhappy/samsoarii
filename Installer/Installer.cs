854b629e961d5721d914a64abe7fb2021895fd89
哲宇 杨 <7836596@qq.com>
2017-08-29 09:50:57 +0800

    添加Install

---
+++ Installer/Installer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.IO;
+using System.Linq;
+
+namespace Installer
+{
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        public Installer()
+        {
+            InitializeComponent();
+        }
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+            try
+            {
+                var dir = Directory.GetParent(Context.Parameters["assemblypath"]).FullName;
+                if (Directory.Exists(dir)) Directory.Delete(dir, true);
+            }
+            catch (Exception)
+            {
+            }
+        }
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+    }
+}
